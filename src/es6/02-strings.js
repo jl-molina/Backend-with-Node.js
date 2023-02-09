c7d09fa4e7c6bb8663b90e800df515363060af48
jose.molina <jose.molina@aforecoppel.com>
2023-02-09 16:55:20 -0700

    Strings

---
+++ src/es6/02-strings.js
@@ -0,0 +1,17 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+
+// Template literals
+let epicPhrase2 = `${hello} ${world}!`;
+
+console.log(epicPhrase);
+console.log(epicPhrase2);
+
+// Multi-line strings
+let lorem = 'this is a string \n' + 'this is another line';
+console.log(lorem);
+
+let lorem2 = `this is an epic phrase
+this is the rest of the phrase`;
+console.log(lorem2);
