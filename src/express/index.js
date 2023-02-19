5e487d3e7191bfaaa0141bd3e12ade64a0141b25
jl-molina <joseluisml1218@gmail.com>
2023-02-19 14:20:36 -0700

    Query Params

--- src/express/index.js
+++ src/express/index.js
@@ -1,59 +1,53 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
+const products = [];
 
-const categories = [
-    {
-        id: 1,
-        name: 'electronics'
-    },
-    {
-        id: 2,
-        name: 'clothes'
-    }
-]
 
-const products = [
-    {
-        id: 1,
-        name: 'product1',
-        price: 1000,
-        category: 1
-    },
-    {
-        id: 1,
-        name: 'product2',
-        price: 2000,
-        category: 2
-    },
-    {
-        id: 2,
-        name: 'product3',
-        price: 2000,
-        category: 2
-    },
-    {
-        id: 2,
-        name: 'product4',
-        price: 2000,
-        category: 1
-    }
-];
+for (let i = 0; i < 100; i++) {
+    products.push({
+        id: i,
+        name: faker.commerce.productName(),
+        price: parseInt(faker.commerce.price(), 10),
+        image: faker.image.imageUrl()
+    })
+}
 
 app.get('/', ((req, res) => res.send('Hello world')));
 
 app.get('/products', ((req, res) => {
-    res.json(products);
+    const { id } = req.query;
+    if (id) {
+        res.json(products[id]);
+    } else {
+        res.json(products);
+    }
 }));
+
+app.get('/products/filter', (req, res) => res.send('Filter'));
 
 app.get('/products/:id', (req, res) => {
     const { id } = req.params;
     res.json(products[id]);
-})
+});
+
 
 app.get('/categories/:id/products/:productID', ((req, res) => {
     const { id, productID } = req.params;
     res.json(products.filter(e => e.category === parseInt(id) && e.id == productID));
 }));
+
+app.get('/users', (req, res) => {
+    const { limit, offset } = req.query;
+    if (limit && offset) {
+        res.json({
+            limit,
+            offset
+        })
+    } else {
+        res.send('No args');
+    }
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
