e5b0774d5df74e3285e7875dde67911619b45b44
jl-molina <joseluisml1218@gmail.com>
2023-03-05 12:25:28 -0700

    Patch, delete and status codes

--- src/express/index.js
+++ src/express/index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const routerApi = require('./routes');
 const app = express();
 const port = 3000;
-const products = [];
 
 app.use(express.json());
 routerApi(app);
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port);
--- src/express/routes/products.router.js
+++ src/express/routes/products.router.js
@@ -25,15 +25,34 @@ router.get('/filter', (req, res) => res.send('Filter'));
 
 router.get('/:id', (req, res) => {
     const { id } = req.params;
-    res.json(products[id]);
+    id >= 100 ? res.status(404).json({message: 'Not found'}) : res.json(products[id]);
 });
 
 router.post('/', (req, res) => {
     const body = req.body;
-    res.json({
+    res.status(201).json({
         message: 'created',
         data: body
     })
+});
+
+router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+    res.json({
+        message: 'update patch',
+        data: body,
+        id
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    const body = req.body;
+    res.json({
+        message: 'deleted',
+        id
+    });
 });
 
 module.exports = router;
